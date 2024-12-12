405a94f109bab4b1f8d742d40dd583d2fc99304e
Leonora Pahirko <leonora.pahirko@lu.lv>
2024-12-12 09:21:14 +0100

    Added GLM fitting functions

--- R/functions.R
+++ R/functions.R
@@ -60,11 +60,57 @@ column_values_to_snake_case <- function(data, columns) {
 #'
 #' @return A data.frame/tibble
 metabolites_to_wider <- function(data) {
-    data |>
-        tidyr::pivot_wider(
-            names_from = metabolite,
-            values_from = value,
-            values_fn = mean,
-            names_prefix = "metabolite_"
-        )
+  data |>
+    tidyr::pivot_wider(
+      names_from = metabolite,
+      values_from = value,
+      values_fn = mean,
+      names_prefix = "metabolite_"
+    )
+}
+
+
+
+#' Title
+#'
+#' @param data The lipidomics data set (wide)
+#' @param metabolite_varible The column of the lipidomics metabolite
+#'
+#' @return A recipe object
+create_recipe_spec <- function(data, metabolite_varible) {
+  recipes::recipe(data) |>
+    recipes::update_role(
+      {{ metabolite_varible }},
+      age,
+      gender,
+      new_role = "predictor"
+    ) |>
+    recipes::update_role(class, new_role = "outcome") |>
+    recipes::step_normalize(tidyselect::starts_with("metabolite_"))
+}
+
+
+#' Create a workflow object
+#'
+#' @param model_specs The model specs
+#' @param recipe_specs The recipe specs
+#'
+#' @return A workflow object
+create_model_workflow <- function(model_specs, recipe_specs) {
+  workflows::workflow() |>
+    workflows::add_model(model_specs) |>
+    workflows::add_recipe(recipe_specs)
+}
+
+
+
+#' Create a tidy output of the model results
+#'
+#' @param workflow_fitted_model The model workflow object that has been fitted
+#'
+#' @return A data frame
+tidy_model_output <- function(workflow_fitted_model) {
+  workflow_fitted_model |>
+    workflows::extract_fit_parsnip() |>
+    broom::tidy(exponentiate = TRUE)
 }
